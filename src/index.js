92e73ec357284872505978d39b3fe0444fb06284
Alex Bepple <ab@Marc-Uwe.local>
2017-08-31 00:09:28 +0200

    . add second counter

--- src/index.js
+++ src/index.js
@@ -4,13 +4,15 @@ import flyd from 'flyd'
 
 import {createCounter} from './flakes/counter'
 
-const counter = createCounter()
+const counter1 = createCounter()
+const counter2 = createCounter()
 
 const App = () => <div>
-  <counter.Counter prefix='Count' />
+  <counter1.Counter prefix='Count 1' />
+  <counter2.Counter prefix='Count 2' />
 </div>
 
 flyd.combine(() => render(
   <App />,
   document.getElementById('root')
-), [counter.count])
+), [counter1.count, counter2.count])
