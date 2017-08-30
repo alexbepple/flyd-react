7537891cc63e02450f0eea1093e882ef2a55fe0c
Alex Bepple <ab@Marc-Uwe.local>
2017-08-30 22:18:23 +0200

    get rid of redux, add flyd

--- src/index.js
+++ src/index.js
@@ -1,24 +1,17 @@
 import React from 'react'
 import {render} from 'react-dom'
-import {createStore, applyMiddleware, compose} from 'redux'
-import {Provider} from 'react-redux'
+import {stream} from 'flyd'
 
-import rootReducer from './state'
-import App from './components/App'
+const counter = stream()
+counter(2)
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
-
-const store = createStore(
-  rootReducer,
-  composeEnhancers(applyMiddleware(
-    // add middlewares here
-  ))
+const Message = ({message}) => (
+  <h1>Hello ({counter()})</h1>
 )
 
 render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <Message message='Hello!' />,
   document.getElementById('root')
 )
 
+counter(3)
--- src/state/message.js
+++
@@ -1,8 +0,0 @@
-import {createReducer, createAction} from 'redux-act'
-
-const initialMessage = 'Hello, world!'
-const noInitialHandlers = {}
-const reducer = createReducer(noInitialHandlers, initialMessage)
-
-export default reducer
-
